f9dae201e7db2ecdb94a3223498c3b137cb09de3
fengmk2 <suqian.yf@antgroup.com>
2023-12-20 00:48:23 +0800

    fix: should return AsyncLocalStorage type

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { AsyncLocalStorage } from 'node:async_hooks';
 
 export const kGALS = Symbol.for('gals#asyncLocalStorage');
 
-export function getAsyncLocalStorage() {
+export function getAsyncLocalStorage<T = any>(): AsyncLocalStorage<T> {
   const g: any = globalThis;
   if (!g[kGALS]) {
-    g[kGALS] = new AsyncLocalStorage();
+    g[kGALS] = new AsyncLocalStorage<T>();
   }
   return g[kGALS];
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -4,9 +4,11 @@ import { getAsyncLocalStorage, kGALS } from '../src/index.js';
 describe('test/index.test.ts', () => {
   it('should only create one als instance', () => {
     const als1 = getAsyncLocalStorage();
-    const als2 = getAsyncLocalStorage();
+    const als2 = getAsyncLocalStorage<object>();
     assert.equal(als1, als2);
     assert.equal(als1, Reflect.get(globalThis, kGALS));
     assert.equal(als2, Reflect.get(global, kGALS));
+    assert.equal(typeof als2.getStore, 'function');
+    assert.equal(als2.getStore(), undefined);
   });
 });
